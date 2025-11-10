c831b8257c70e6d5df2c72cf78a0fe56ad330004
Astrum-07 <astrumastro07@gmail.com>
2025-11-10 06:27:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center animate-bounce mt-20 text-4xl'>14:00 gacha yuklab qo'yaman</div>
+  )
+}
+
+export default App
